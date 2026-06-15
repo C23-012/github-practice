0b9c4ce9a2fae6fb4557a3a46d7c2884b6308469
suntech\c23012 <c23012@suntech.jp>
2026-06-15 14:58:33 +0900

    プログラムに例外処理を追加

--- sample.java
+++ sample.java
@@ -2,6 +2,11 @@ public class sample {
     public static void main(String[] args) {
         int a = 10;
         int b = 0;
-        System.out.println(a/b);
+
+        if (b == 0) {
+            System.out.println("エラー: 0で割ることはできません。");
+        } else {
+            System.out.println(a / b);
+        }
     }
 }
