d567297a4c0959105acba9ec85f883f687d58215
suntech\c23012 <c23012@suntech.jp>
2026-06-15 15:02:57 +0900

    Javaプログラムの修正 #4

--- sample.java
+++ sample.java
@@ -3,7 +3,7 @@ public class sample {
         int a = 10;
         int b = 0;
 
-        if (b == 0) {
+        if (b == 0) {//例外処理
             System.out.println("エラー: 0で割ることはできません。");
         } else {
             System.out.println(a / b);
